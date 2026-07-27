78098e110d01c067a11a628e902f7b5b6fcfd5cb
Marylizr <marylizr@gmail.com>
2026-07-27 15:40:16 +0100

    logo update

--- client/src/config/brand.js
+++ client/src/config/brand.js
@@ -1,6 +1,6 @@
 export const APP_NAME = 'GlutenFri';
 export const APP_LOGO_URL =
-  'https://res.cloudinary.com/doroh5hbv/image/upload/v1784991659/logo-glutenFri_mxse6m.webp';
+  'https://res.cloudinary.com/doroh5hbv/image/upload/v1785147880/logo-glutenFri_mxse6m.webp';
 export const APP_ICON_URL =
   'https://res.cloudinary.com/doroh5hbv/image/upload/v1784991668/icon-glutenFri_ilsloa.webp';
 export const REGION_NAME = 'Norte de Portugal';
